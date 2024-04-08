276088489c50f51359138aabe17aaeec88531b8f
unknown <ezequiel.beserra@qualiconsig.com.br>
2024-04-08 15:55:53 -0300

    so funciona

--- src/pages/calculator/dadosSistema/index.tsx
+++ src/pages/calculator/dadosSistema/index.tsx
@@ -1,7 +1,7 @@
 
-import { RealBalance } from "@/compontents/calculatorSide/DataSistema";
-import { BalanceDataSystem } from "@/compontents/calculatorSide/SaldoReal";
-import { Portabilidade } from "@/compontents/resultSide/Portabilidade";
+import { RealBalance } from "../../../compontents/calculatorSide/DataSistema";
+import { BalanceDataSystem } from "../../../compontents/calculatorSide/SaldoReal";
+import { Portabilidade } from "../../../compontents/resultSide/Portabilidade";
 import { Box, Flex, Text } from "@chakra-ui/react";
 
 export default function Calculator () {
