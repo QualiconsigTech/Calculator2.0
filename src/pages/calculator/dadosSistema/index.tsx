f44bc80e4fec90ecf264140a42941175d92753e6
unknown <ezequiel.beserra@qualiconsig.com.br>
2024-04-09 09:35:35 -0300

    deploy fix

--- src/pages/calculator/dadosSistema/index.tsx
+++ src/pages/calculator/dadosSistema/index.tsx
@@ -1,20 +1,13 @@
-
-
-
-import { BalanceDataSystem } from "@/compontents/calculatorSide/SaldoReal";
-import { Portabilidade } from "@/compontents/resultSide/Portabilidade";
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { BalanceDataSystem } from "../../../compontents/calculatorSide/SaldoReal";
+import { Portabilidade } from "../../../compontents/resultSide/Portabilidade";
+import { Box, Flex } from "@chakra-ui/react";
 
 export default function Calculator () {
 
-
     return (
         <Box>
-            <Flex flexDir={['column', 'column','column', 'row']}>
-                
-                
-                <BalanceDataSystem/>
-                
+            <Flex flexDir={['column', 'column','column', 'row']}> 
+                <BalanceDataSystem/>     
                 <Portabilidade/>
             </Flex>
         </Box>
