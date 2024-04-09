cbe53e4cc0dc3cf1e0443422b104fad8375e06e7
unknown <ezequiel.beserra@qualiconsig.com.br>
2024-04-09 09:16:52 -0300

    deploy fix

--- src/pages/calculator/dadosSistema/index.tsx
+++ src/pages/calculator/dadosSistema/index.tsx
@@ -1,8 +1,8 @@
 
+
+
 import { BalanceDataSystem } from "@/compontents/calculatorSide/SaldoReal";
 import { Portabilidade } from "@/compontents/resultSide/Portabilidade";
-
-
 import { Box, Flex, Text } from "@chakra-ui/react";
 
 export default function Calculator () {
