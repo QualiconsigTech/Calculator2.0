f43f4b422e4cf99d92534fa3699a8680f5301f6b
unknown <ezequiel.beserra@qualiconsig.com.br>
2024-04-12 12:19:46 -0300

    add analytic

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react"
 
 import { ChakraProvider } from '@chakra-ui/react'
 import { MathInbursa } from "@/Context/InbursaContext";
 
 
 export default function App({ Component, pageProps }: AppProps) {
-  return  <ChakraProvider><MathInbursa><Component {...pageProps} /></MathInbursa></ChakraProvider>
+  return  <ChakraProvider> <Analytics /><MathInbursa><Component {...pageProps} /></MathInbursa></ChakraProvider>
 }
